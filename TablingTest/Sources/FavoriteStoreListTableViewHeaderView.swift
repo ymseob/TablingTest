7e0430a2200f70fddfd4abdc408223b67e9f2331
Minseob Yoon <workerorion@naver.com>
2022-05-14 11:49:01 +0900

    HeaderView update UI 로직 구현

--- TablingTest/Sources/FavoriteStoreListTableViewHeaderView.swift
+++ TablingTest/Sources/FavoriteStoreListTableViewHeaderView.swift
@@ -8,12 +8,67 @@
 import UIKit
 
 final class FavoriteStoreListTableViewHeaderView: UITableViewHeaderFooterView {
+    @IBOutlet private weak var savedLabel: UILabel!
+    @IBOutlet private weak var recentlyLabel: UILabel!
+    @IBOutlet private weak var bottomIndicatorView: UIView!
+    @IBOutlet private weak var bottomIndicatorViewWidthConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var bottomIndicatorLeadingConstraint: NSLayoutConstraint!
+
+    enum ListType {
+        case saved
+        case recently
+    }
+
+    private let savedLabelTapGestureRecognizer = UITapGestureRecognizer()
+    private let recentlyLabelTapGestureRecognizer = UITapGestureRecognizer()
+
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupBackgroundView()
+        setupGestures()
+    }
+
+    private func setupBackgroundView() {
         backgroundView = {
             let view = UIView(frame: self.bounds)
             view.backgroundColor = .white
             return view
         }()
+    }
+
+    private func setupGestures() {
+        savedLabel.addGestureRecognizer(savedLabelTapGestureRecognizer)
+        savedLabelTapGestureRecognizer.addTarget(self, action: #selector(savedDidTap))
+
+        recentlyLabel.addGestureRecognizer(recentlyLabelTapGestureRecognizer)
+        recentlyLabelTapGestureRecognizer.addTarget(self, action: #selector(recentlyDidTap))
+    }
+
+    private func updateUI(to listType: ListType) {
+        guard let willFoucusLabel = listType == .saved ? savedLabel : recentlyLabel else {
+            return
+        }
+
+        let selected: UIColor = .black
+        let deselected: UIColor = .lightGray
+        savedLabel.textColor = willFoucusLabel == savedLabel ? selected : deselected
+        recentlyLabel.textColor = willFoucusLabel == recentlyLabel ? selected : deselected
+
+        UIViewPropertyAnimator(duration: 0.2, curve: .linear) {
+            self.bottomIndicatorViewWidthConstraint.constant = willFoucusLabel.bounds.width
+            self.bottomIndicatorLeadingConstraint.constant = willFoucusLabel.frame.origin.x
+            self.layoutIfNeeded()
+        }.startAnimation()
+    }
+
+
+    @objc
+    private func savedDidTap() {
+        updateUI(to: .saved)
+    }
+
+    @objc
+    private func recentlyDidTap() {
+        updateUI(to: .recently)
     }
 }
